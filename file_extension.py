c6ca38fa77b23669f54206fbee7f92123d82a5c1
Oceanicrasin <107171751+Oceanicrasin@users.noreply.github.com>
2023-01-06 12:46:13 +0000

    Create file_extension.py

---
+++ file_extension.py
@@ -0,0 +1,16 @@
+file_name = input("File name: ")
+
+if file_name.endswith(".jpg"):
+    print("image/jpg")
+if file_name.endswith(".jpeg"):
+    print("image/jpeg")
+if file_name.endswith(".gif"):
+    print("image/gif")
+if file_name.endswith(".png"):
+    print("image/png")
+if file_name.endswith(".pdf"):
+    print("application/pdf")
+if file_name.endswith(".txt"):
+    print("txt/txt")    
+if file_name.endswith(".zip"):
+    print("application/zip")    
